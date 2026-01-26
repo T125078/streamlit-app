d31eb0e8bab2a3457ce12d0398cca84d9486ffc2
T125078 <T125078@ed.sus.ac.jp>
2026-01-26 11:20:19 +0900

    1.0.21

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ https://www.e-stat.go.jp/stat-search/files?page=1&layout=datalist&toukei=0010040
 https://www.e-stat.go.jp/stat-search/files?page=1&layout=datalist&cycle=2&toukei=00100409&tstat=000001014470&tclass1=000001014471&tclass2=000001018314&tclass3val=0&stat_infid=000040385214
 
 データ取得日 : 2026年1月26日
+
+(単位:10億円)
--- app.py
+++ app.py
@@ -13,9 +13,13 @@ components.html("<script>alert('ようこそ')</script>")
 
 st.title("Webアプリを作ってみた!")
 
+#使い方
 st.subheader("使い方")
 how_to_use = st.selectbox("使い方を知りたい項目を選んでください",["AAA","BBB"])
 if st.button("使い方確認"):
   components.html(f"<script>alert('{how_to_use}の使い方を確認します。')</script>")
 
+
+#データフレーム作成
 df = pd.read_csv("gaku-mg2532.csv")
+st.dataframe(df,width=800,height=220)
