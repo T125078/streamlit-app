42cb9666e946daa78e7068687c61ac81e4c87a72
T125078 <T125078@ed.sus.ac.jp>
2026-01-26 10:22:37 +0900

    1.0.1

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
 import time
+import streamlit.components.v1 as components
 
-#3秒間待機
+#1秒間待機
 with st.spinner("Wait for it..."):
-  time.sleep(3)
+  time.sleep(1)
+
+components.html("<script>alert('ようこそ')</script>")
 
 st.title("Webアプリを作ってみた!")
